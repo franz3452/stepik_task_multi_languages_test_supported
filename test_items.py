2770d5402938471f13f7ff6948c42151314c2b49
frantsev_da <franz3452@gmail.com>
2021-01-17 20:55:22 +0300

    добавил второй линк

--- test_items.py
+++ test_items.py
@@ -3,10 +3,11 @@ import time
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#тут нет кнопки link = 'http://selenium1py.pythonanywhere.com/catalogue/hackers-painters_185/'
 
 class TestGoodPage():
     def test_btn_add_good_to_cart_exists(self, browser):
         browser.get(link)
         #time.sleep(10)
-        btn_add_to_cart = len(browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket'))
-        assert btn_add_to_cart == 1, 'Элемент не найден'
+        btn_add_to_cart = len(browser.find_elements(By.CSS_SELECTOR, '.btn.btn-primary.btn-add-to-basket'))
+        assert btn_add_to_cart == 1, 'Кнопка добавления в коорзину не найдена'
